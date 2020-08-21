67df4ed6b2dc01f8a4e7578e6f84383bf45658ef
Geoff Hulette <geoff@hulette.net>
2020-08-21 14:36:30 -0700

    More stuff

--- Monoid.v
+++ Monoid.v
@@ -19,6 +19,7 @@ Module Type Monoid.
 
 End Monoid.
 
+
 Module NatAddMonoid <: Monoid.
   Definition t := nat.
   Definition mempty := 0.
@@ -43,6 +44,33 @@ Module NatAddMonoid <: Monoid.
   Qed.
 
 End NatAddMonoid.
+
+
+Module NatMultMonoid <: Monoid.
+  Definition t := nat.
+  Definition mempty := 1.
+  Definition mappend := mult.
+
+  Theorem mempty_left_identity :
+    forall m, mappend mempty m = m.
+  Proof.
+    apply Nat.mul_1_l.
+  Qed.
+
+  Theorem mempty_right_identity :
+    forall m, mappend m mempty = m.
+  Proof.
+    apply Nat.mul_1_r.
+  Qed.
+
+  Theorem mappend_assoc :
+    forall m n o, mappend m (mappend n o) = mappend (mappend m n) o.
+  Proof.
+    apply Nat.mul_assoc.
+  Qed.
+
+End NatMultMonoid.
+
 
 Module Type Value.
   Parameter t : Type.
@@ -73,3 +101,43 @@ Module ListMonoid (V : Value) <: Monoid.
   Qed.
 
 End ListMonoid.
+
+
+Module Reduce (M : Monoid).
+
+  Fixpoint reduce (ms : list M.t) : M.t :=
+    match ms with
+    | [] => M.mempty
+    | m::ms' => M.mappend m (reduce ms')
+    end.
+
+    Theorem reduce_one :
+      forall m, reduce [m] = m.
+    Proof.
+      intros m.
+      simpl.
+      apply M.mempty_right_identity.
+    Qed.
+
+    Theorem reduce_app :
+      forall l1 l2,
+        reduce (l1 ++ l2) = M.mappend (reduce l1) (reduce l2).
+    Proof.
+      intros l1 l2.
+      induction l1; simpl.
+      - symmetry.
+        apply M.mempty_left_identity.
+      - rewrite IHl1.
+        apply M.mappend_assoc.
+    Qed.
+
+End Reduce.
+
+
+Module NatAddReduce := Reduce(NatAddMonoid).
+Definition sum := NatAddReduce.reduce.
+Compute (sum [1;2;3;4;5]).
+
+Module NatMultReduce := Reduce(NatMultMonoid).
+Definition product := NatMultReduce.reduce.
+Compute (product [1;2;3;4;5]).
